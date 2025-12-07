270f1c0a6c38e007ebc96346df7eae9e1a91d399
bitc0der <59016822+bitc0der@users.noreply.github.com>
2025-12-07 18:14:06 +0700

    Use read-only span

--- src/Grip.Data/Infrastructure/BinaryDeserializer.cs
+++ src/Grip.Data/Infrastructure/BinaryDeserializer.cs
@@ -5,12 +5,10 @@ namespace Grip.Data.Infrastructure;
 
 public static class BinaryDeserializer
 {
-    public static T Deserialize<T>(byte[] buffer)
+    public static T Deserialize<T>(ReadOnlySpan<byte> span)
         where T : struct
     {
-        ArgumentNullException.ThrowIfNull(buffer);
-
-        var handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        var handle = GCHandle.Alloc(span.GetPinnableReference(), GCHandleType.Pinned);
         try
         {
             return Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
