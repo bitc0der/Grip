4580c6b187f91b267f17676fa4bda90fb5941745
bitc0der <59016822+bitc0der@users.noreply.github.com>
2025-12-07 12:57:16 +0700

    Add PacketCarSetupData

---
+++ src/Grip.Data/Definition/PacketCarSetupData.cs
@@ -0,0 +1,30 @@
+using System.Runtime.InteropServices;
+
+namespace Grip.Data.Definition;
+
+/// <summary>
+/// This packet details the car setups for each vehicle in the session. Note that in multiplayer games, other player cars will appear as blank, you will only be able to see your own car setup, regardless of the “Your Telemetry” setting. Spectators will also not be able to see any car setups.
+///
+/// Frequency: 2 per second
+/// Size: 1133 bytes
+/// Version: 1
+/// </summary>
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct PacketCarSetupData
+{
+    /// <summary>
+    /// Header
+    /// </summary>
+    public PacketHeader Header;
+
+    /// <summary>
+    /// Car setup data for all cars
+    /// </summary>
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+    public CarSetupData[] CarSetups;
+
+    /// <summary>
+    /// Value of front wing after next pit stop - player only
+    /// </summary>
+    public float NextFrontWingValue;
+}
